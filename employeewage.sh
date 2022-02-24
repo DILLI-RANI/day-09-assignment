cd468d1a9acb691f6b67881fafff071e1be12060
P DILLI RANI <pasupulatidillirani@gmail.com>
2022-02-24 10:32:02 +0530

    employee:

---
+++ employeewage.sh
@@ -0,0 +1,64 @@
+#Welcome  to Employee Wage Computation program
+
+#Checking regular employee is present or absent
+echo "1-for regular employee, 0-for part time employee"
+read -p "enter the number to know about employee:" num
+monthly_wage=0
+weekend=$((20*8*10))
+i=0
+day=0
+hours=0
+case $num in
+	1) for((i=0;i<=20;i++))
+	do
+		if(($((RANDOM%2))==0))
+		then
+				daily_wage=$((20*8))
+				monthly_wage=$((monthly_wage+$daily_wage))
+					salary[$i]=$daily_wage
+					day1[$i]=$i
+               			((day++))
+                		hours=$((hours+8))
+		else
+				salary[$i]=0
+				day1[$i]=$i
+
+		fi
+	done
+		echo "day - daily_wage"
+		for((i=0;i<=20;i++))
+		do
+			echo ${day1[i]}" - " ${salary[i]}
+		done
+			echo "working hours in a month are: " $hours
+			echo " monthly wage of the regular employee is:" $(($monthly_wage+$weekend))
+	;;
+#checking parttime employee is present or absent
+
+	2)  for((i=0;i<=20;i++))
+        do
+		if(($((RANDOM%2))==0))
+        	then
+                                daily_wage=$((20*8))
+                                monthly_wage=$((monthly_wage+$daily_wage))
+                        		salary[$i]=$daily_wage
+					day1[$i]=$i
+                                ((day++))
+                                hours=$((hours+8))
+		else
+                		salary[$i]=0
+				day1[$i]=$i
+        	fi
+	done
+                echo "day - daily_wage"
+                for((i=0;i<=20;i++))
+                do
+                        echo ${day1[i]}" - " ${salary[i]}
+                done
+
+			echo "woking hours in a month are:" $hours
+                        echo "monthly wage of the part time employee is:" $(($monthly_wage+$weekend))
+	;;
+	*) echo "invalid input - number must be 0 or 1"
+esac
+© 2022 GitHub, Inc.
